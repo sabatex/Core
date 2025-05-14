1f1d9318f03e06c0db3a7fce297cb68ec3ea3a54
sabatex <serhiy.lakas@hotmail.com>
2025-05-14 09:27:07 +0300

    fix changes for reorganization (code not compile)

---
+++ UploadObject.cs
@@ -0,0 +1,18 @@
+﻿using Sabatex.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabatex.ObjectExchange.Core;
+
+public class UploadObject:IEntityBase<Guid>
+{
+    public Guid Id { get; set; }
+    public ExchangeNode Node { get; set; }
+    public Guid NodeId { get; set; }
+    public string MessageHeader { get; set; }
+    public string Message { get; set; }
+    public DateTime DateStamp { get; set; }
+}
