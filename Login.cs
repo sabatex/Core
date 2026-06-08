4e656f7be43bdba63c47014d96f640d8eab6ff9a
sabatex <serhiy.lakas@hotmail.com>
2026-06-08 10:31:09 +0300

    No code changes detected
    No actual code changes were made; only instructions and context were present in the diff.

---
+++ Login.cs
@@ -0,0 +1,16 @@
+﻿namespace Sabatex.ObjectsExchange.Models;
+using System;
+/// <summary>
+/// Login data class
+/// </summary>
+public class Login
+{
+    /// <summary>
+    /// client id
+    /// </summary>
+    public Guid ClientId { get; set; }
+    /// <summary>
+    /// Client password
+    /// </summary>
+    public string Password { get; set; }  = default!;
+}
