7bba01417d12d54e5bc879c8ecae395b57b5f478
sabatex <serhiy.lakas@hotmail.com>
2026-06-08 12:01:35 +0300

    #
    Add ObjectExtensions with FillAttributes and bump version
    
    Added ObjectExtension.cs with ObjectExtensions class and FillAttributes<T> extension method for copying property values by name using reflection. Updated project version to 10.2.1.

---
+++ Sabatex.Core/ClassExtensions/ObjectExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabatex.Core.ClassExtensions;
+/// <summary>
+/// Реалізація методу розширення для заповнення атрибутів одного об'єкта значеннями атрибутів іншого об'єкта.
+/// </summary>
+public static class ObjectExtensions
+{
+    /// <summary>
+    /// Розширення для заповнення атрибутів одного об'єкта значеннями атрибутів іншого об'єкта.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    public static T FillAttributes<T>(this T obj, object source) where T : class
+    {
+        foreach (var property in source.GetType().GetProperties())
+        {
+            var objAttr = obj.GetType().GetProperty(property.Name);
+            if (objAttr != null)
+            {
+                objAttr.SetValue(obj, property.GetValue(source, null), null);
+            }
+        }
+        return obj;
+    }
+}
