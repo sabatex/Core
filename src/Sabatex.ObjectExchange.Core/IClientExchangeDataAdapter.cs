0f26bb503bf9a2d2c8deb682b5ab078fa2595d49
Serhiy Lakas <serhiy.lakas@hotmail.com>
2026-06-20 13:43:48 +0300

    Update version and extend IClientExchangeDataAdapter API
    - Bump project version to 10.5.0-rc6 in Directory.Build.props
    - Add methods for removing and retrieving upload/unresolved messages to IClientExchangeDataAdapter
    - Improve XML documentation for interface methods
    - Remove GetExchangeNodesAsync from the interface

--- src/Sabatex.ObjectExchange.Core/IClientExchangeDataAdapter.cs
+++ src/Sabatex.ObjectExchange.Core/IClientExchangeDataAdapter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,9 +15,19 @@ public interface IClientExchangeDataAdapter
     /// <param name="messageHeader">Заголовк повідомлення</param>
     /// <param name="message">Саме повідомлення, для запитів може бути відсутнє</param>
     /// <returns></returns>
-
     Task RegisterUploadMessageAsync(Guid nodeId, string messageHeader, string message);
+    /// <summary>
+    /// Видалення повідомлення після обробки.
+    /// </summary>
+    /// <param name="id">Ідентифікатор повідомлення</param>
+    /// <returns></returns>
     Task RemoveUploadMessageAsync(Guid id);
+    /// <summary>
+    /// Отримати список повідомлень для відправки.
+    /// </summary>
+    /// <param name="destinationNode">Нод призначення</param>
+    /// <param name="take">Кількість повідомлень для отримання</param>
+    /// <returns>Список повідомлень</returns>
     Task<IEnumerable<UploadObject>> GetUploadMessagesAsync(Guid destinationNode, int take = 10);
     /// <summary>
     /// Реєстрація повідомлення, яке ще не обробилось.
@@ -26,12 +36,23 @@ public interface IClientExchangeDataAdapter
     /// <param name="unresolvedObject">вхідний обєкт/запит який буде аналізуватись</param>
     /// <returns></returns>
     Task RegisterUnresolvedMessageAsync(Guid nodeId, UnresolvedObject unresolvedObject);
+    /// <summary>
+    /// Видалення необробленого повідомлення після обробки.
+    /// </summary>
+    /// <param name="id">Ідентифікатор повідомлення</param>
+    /// <returns></returns>
     Task RemoveUnresolvedMessageAsync(Guid id);
+    /// <summary>
+    /// Отримати список необроблених повідомлень.
+    /// </summary>
+    /// <param name="nodeId">DestinationId нода</param>
+    /// <param name="take">Кількість повідомлень для отримання</param>
+    /// <returns>Список необроблених повідомлень</returns>
     Task<IEnumerable<UnresolvedObject>> GetUnresolvedMessagesAsync(Guid nodeId, int take = 10);
     /// <summary>
     /// Реєстрація статусу обробки повідомлення.
     /// </summary>
-    /// <param name="nodeId"></param>
+    /// <param name="nodeId">DestinationId нода</param>
     /// <param name="id"></param>
     /// <param name="state">Статус обробки</param>
     /// <returns></returns>
@@ -43,11 +64,4 @@ public interface IClientExchangeDataAdapter
     /// <param name="messageHeader">заголовок повідомлення</param>
     /// <returns>true - message exist</returns>
     Task<bool> CheckExistUnresolvedMessage(Guid nodeId, string messageHeader);
-
-    /// <summary>
-    /// Отримати список вузлів обміну даними.
-    /// </summary>
-    /// <returns>Список активних вузлів обміну</returns>
-    Task<IEnumerable<ExchangeNode>> GetExchangeNodesAsync(bool onlyActive = false);
-
 }
