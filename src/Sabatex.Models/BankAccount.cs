0b4952ad93e0fc1d582977e9de13da23295f4068
sabatex <serhiy.lakas@hotmail.com>
2026-09-04 10:02:55 +0300

    Bump versions, add Sabatex.Models project, update URLs
    - Updated central package versions in Directory.Packages.props
    - Bumped solution version to 10.11.0-rc1 in Directory.Build.props
    - Added Sabatex.Models project with BankAccount model (IBAN, Base1CCatalog)
    - Registered Sabatex.Models in sabatex-publish-solution.json
    - Updated repository URLs in project files to https://github.com/sabatex/Core

---
+++ src/Sabatex.Models/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using Sabatex.Core;
+
+namespace Sabatex.Models;
+/// <summary>
+/// Represents a bank account with an International Bank Account Number (IBAN).
+/// </summary>
+public class BankAccount : Base1CCatalog
+{
+    /// <summary>
+    /// Gets or sets the International Bank Account Number (IBAN) of the bank account.
+    /// </summary>
+    public string IBAN { get; set; } = string.Empty;
+
+}
